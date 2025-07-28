1212446e75dc4c0502e02008a122d9e6ed938b20
kamilrybacki <kamil.rybacki@nordsec.com>
2025-07-28 17:32:27 +0200

    feat: Add setup type stubs and wiring functions for SDK service dependencies

---
+++ src/nexosapi/config/setup.pyi
@@ -0,0 +1,24 @@
+import dataclasses
+from dependency_injector.containers import DynamicContainer
+from dependency_injector.providers import Provider
+from enum import StrEnum
+from nexosapi.services.http import NexosAIAPIService as NexosAIAPIService
+
+class ServiceName(StrEnum):
+    NEXOSAI_API_HTTP_CLIENT = "NexosAIAPIClient"
+
+@dataclasses.dataclass(kw_only=True)
+class WiringDictionaryEntry:
+    service_class: type
+    provider_class: type[Provider]
+    modules: set[str]
+
+WIRING: dict[str, WiringDictionaryEntry]
+SDK_SERVICES_CONTAINER: DynamicContainer
+
+def populate_container(container: DynamicContainer, providers_config: dict[str, WiringDictionaryEntry]) -> set[str]: ...
+def wire_skd_dependencies() -> None:
+    """
+    Wire the SDK dependencies.
+    This function is called to ensure that the SDK services are properly initialized.
+    """
